db031c7c9d3824df41c0070a15446b01165fc04e
Sandeepkumardwibei <sujatadwibei700@gmail.com>
2025-07-21 14:14:59 +0530

    Add_solutions_for_practice_problems

---
+++ WEEK5/File3-microservices-composite-handson/Code/api-gateway/src/main/java/com/cognizant/apigateway/filter/LogFilter.java
@@ -0,0 +1,23 @@
+package com.cognizant.apigateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+@Order(1)
+public class LogFilter implements GlobalFilter {
+    private final Logger logger = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Request path: {}", exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+}
